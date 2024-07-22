9068a56940e4b8ac28b50e0e4af7d4682ff4ffd4
leo <leojwgulliver@gmail.com>
2024-07-22 20:24:01 +0100

    add function for computerchoice

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("hello world")
+console.log("hello world");
+// let answer = prompt("type smth: ");
+//console.log(answer);
+
+let objects = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+    let rand = Math.random();
+    if (rand <= 0.3) {
+        return objects[0];
+    } else if (rand > 0.3 && rand <= 0.6) {
+        return objects[1];
+    } else {
+        return objects[2];
+    }
+}
+
+alert(getComputerChoice());
